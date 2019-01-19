619ef253fb56a5ee7b3a195b52cce4a69f427e2b
Aanand Madhav <aanand.madhav36@gmail.com>
2019-01-19 11:59:22 +0530

    initial commit

---
+++ index7b17.php
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<?xml-stylesheet href="/basanti/components/com_imageshow/assets/css/style.css" type="text/css"?>
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Home</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://caforstartups.com"/>
+	<id>http://caforstartups.com/basanti/index.php</id>
+	<updated>2019-01-18T19:33:01+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://caforstartups.com/basanti/basanti/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>Peace on Earth</title>
+		<link rel="alternate" type="text/html" href="http://caforstartups.com/basanti/index.php/61-home/15-peace-on-earth-english"/>
+		<published>2011-03-31T03:25:00+00:00</published>
+		<updated>2011-03-31T03:25:00+00:00</updated>
+		<id>http://caforstartups.com/basanti/index.php/61-home/15-peace-on-earth-english</id>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;h2&gt;Peace on Earth&lt;/h2&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;Like every other discipline, religion has its own particular Province and a particular function. If we were to forget this function and let it take up other functions, we shall be destroying religion. Unfortunately, that is what is happening in many places.&lt;/p&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;Keeping in mind the basic function of religion it is absolutely essential for us, if at all we are interested in the religious pursuit, that we establish ourselves in the Consciousness of our own True Self. The more we do so, the more peace will come to this world. The more we do so, the more we shall find that we have solved our own problems, the problems of our fellow human beings and the problems of the world.&lt;/p&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;In case we do not establish ourselves in the Consciousness of our own True Self we shall make a mess of everything. This world, our common home, would be like a lunatic asylum with over six billion unregistered insane occupants, with no professionals to man it.&lt;/p&gt;
+&lt;p style=&quot;font-size: 16px; color: #666; text-align: justify;&quot;&gt;&lt;strong&gt;&lt;em&gt;So to serve the humanity we owe it to ourselves and to the future generations that we make a concerted effort to firmly establish ourselves in the Consciousness of our own True Self.&lt;/em&gt;&lt;/strong&gt;&lt;/p&gt;
+&lt;p&gt;&lt;a href=&quot;http://caforstartups.com/basanti/index.php?option=com_content&amp;amp;view=article&amp;amp;id=86:peace-on-earth-hindi&amp;amp;catid=61:home&amp;amp;Itemid=101&quot;&gt;View in Hindi&lt;/a&gt;&lt;/p&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;h2&gt;Peace on Earth&lt;/h2&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;Like every other discipline, religion has its own particular Province and a particular function. If we were to forget this function and let it take up other functions, we shall be destroying religion. Unfortunately, that is what is happening in many places.&lt;/p&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;Keeping in mind the basic function of religion it is absolutely essential for us, if at all we are interested in the religious pursuit, that we establish ourselves in the Consciousness of our own True Self. The more we do so, the more peace will come to this world. The more we do so, the more we shall find that we have solved our own problems, the problems of our fellow human beings and the problems of the world.&lt;/p&gt;
+&lt;p style=&quot;text-align: justify;&quot;&gt;In case we do not establish ourselves in the Consciousness of our own True Self we shall make a mess of everything. This world, our common home, would be like a lunatic asylum with over six billion unregistered insane occupants, with no professionals to man it.&lt;/p&gt;
+&lt;p style=&quot;font-size: 16px; color: #666; text-align: justify;&quot;&gt;&lt;strong&gt;&lt;em&gt;So to serve the humanity we owe it to ourselves and to the future generations that we make a concerted effort to firmly establish ourselves in the Consciousness of our own True Self.&lt;/em&gt;&lt;/strong&gt;&lt;/p&gt;
+&lt;p&gt;&lt;a href=&quot;http://caforstartups.com/basanti/index.php?option=com_content&amp;amp;view=article&amp;amp;id=86:peace-on-earth-hindi&amp;amp;catid=61:home&amp;amp;Itemid=101&quot;&gt;View in Hindi&lt;/a&gt;&lt;/p&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Home" />
+	</entry>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<?xml-stylesheet href="/basanti/components/com_imageshow/assets/css/style.css" type="text/css"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Home</title>
+		<description><![CDATA[]]></description>
+		<link>http://caforstartups.com/basanti/index.php</link>
+		<lastBuildDate>Fri, 18 Jan 2019 19:33:00 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://caforstartups.com/basanti/basanti/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+		<item>
+			<title>Peace on Earth</title>
+			<link>http://caforstartups.com/basanti/index.php/61-home/15-peace-on-earth-english</link>
+			<guid isPermaLink="true">http://caforstartups.com/basanti/index.php/61-home/15-peace-on-earth-english</guid>
+			<description><![CDATA[<div class="feed-description"><h2>Peace on Earth</h2>
+<p style="text-align: justify;">Like every other discipline, religion has its own particular Province and a particular function. If we were to forget this function and let it take up other functions, we shall be destroying religion. Unfortunately, that is what is happening in many places.</p>
+<p style="text-align: justify;">Keeping in mind the basic function of religion it is absolutely essential for us, if at all we are interested in the religious pursuit, that we establish ourselves in the Consciousness of our own True Self. The more we do so, the more peace will come to this world. The more we do so, the more we shall find that we have solved our own problems, the problems of our fellow human beings and the problems of the world.</p>
+<p style="text-align: justify;">In case we do not establish ourselves in the Consciousness of our own True Self we shall make a mess of everything. This world, our common home, would be like a lunatic asylum with over six billion unregistered insane occupants, with no professionals to man it.</p>
+<p style="font-size: 16px; color: #666; text-align: justify;"><strong><em>So to serve the humanity we owe it to ourselves and to the future generations that we make a concerted effort to firmly establish ourselves in the Consciousness of our own True Self.</em></strong></p>
+<p><a href="http://caforstartups.com/basanti/basanti/index.php/61-home/86-peace-on-earth-hindi">View in Hindi</a></p></div>]]></description>
+			<category>Featured</category>
+			<category>Home</category>
+			<pubDate>Thu, 31 Mar 2011 03:25:00 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
